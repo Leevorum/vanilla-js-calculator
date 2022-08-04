3331b748d9e06c41cd389fd15c4dc29d068239f3
Leevorum <di4111ua@gmail.com>
2022-08-04 23:24:24 +0300

    fixes and separeta code by modules

--- src/js/refs.js
+++ src/js/refs.js
@@ -11,4 +11,25 @@ function getRefs() {
   };
 }
 
+export const calcState = {
+  firstNumber: '',
+  secondNumber: '',
+  sign: '',
+  result: '',
+  accumulator: '',
+};
+export const functionsArray = ['Clear', 'Enter', '=', '/', '*', '-', '+', '%'];
+export const numbersArray = [
+  '1',
+  '2',
+  '3',
+  '4',
+  '5',
+  '6',
+  '7',
+  '8',
+  '9',
+  '0',
+  '.',
+];
 export default getRefs;
